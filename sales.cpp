9b329e8600863d2374ffec34e1cf984d58a27be7
Mohanapriya2828 <mohanapriya.jayabalan@psiog.com>
2025-08-19 14:50:24 +0530

    Second commit

--- sales.cpp
+++ sales.cpp
@@ -48,6 +48,27 @@ int getNextId(const string& filename) {
     return maxId + 1;
 }
 
+bool isValidDate(const string& date) {
+    if (date.size() != 10) return false;
+    if (date[2] != '/' || date[5] != '/') return false;
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 2 || i == 5) continue;
+        if (!isdigit(date[i])) return false;
+    }
+
+    int day = stoi(date.substr(0, 2));
+    int month = stoi(date.substr(3, 2));
+    int year = stoi(date.substr(6, 4));
+
+    if (year < 1000 || year > 9999) return false; 
+    if (month < 1 || month > 12) return false;
+    if (day < 1 || day > 31) return false;
+
+
+    return true;
+}
+
 void appendSaleToCSV(const string& filename, const Sale& s) {
     bool writeHeader = !filesystem::exists(filename) || isFileEmpty(filename);
 
@@ -81,8 +102,13 @@ int main() {
         Sale s;
         s.id = nextId++;
 
-        cout << "Enter date (YYYY-MM-DD): ";
-        cin >> s.date;
+        // Date validation loop
+        do {
+            cout << "Enter date (DD/MM/YYYY): ";
+            cin >> s.date;
+            if (!isValidDate(s.date))
+                cout << "Invalid date, try again.\n";
+        } while (!isValidDate(s.date));
 
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear buffer
 
